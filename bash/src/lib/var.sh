e7fb70cc634db4f2b08eb3c5c76a1670f6ed17ad
Your Name <you@example.com>
2026-03-27 20:53:51 +0000

    try my

--- bash/src/lib/var.sh
+++ bash/src/lib/var.sh
@@ -1,5 +1,11 @@
-. lib/var.default
+. var/var.default
 
+# If present, load example
+if [ -f my/var.example ]; then
+        . my/var.example
+fi
+
+# Load user last to override and defaults
 if [ -f my/var ]; then
         . my/var
 fi
